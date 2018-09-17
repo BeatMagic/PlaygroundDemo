906ff4aab1785bba07f1b76696111a07deaff58e
seaniezhao <zwxbest@hotmail.com>
2018-09-17 12:36:53 +0800

    音色枚举

---
+++ PlaygroundDemo/PlaygroundDemo/Class/AppStandard/Timbres.swift
@@ -0,0 +1,14 @@
+//
+//  Timbres.swift
+//  PlaygroundDemo
+//
+//  Created by apple on 2018/9/17.
+//  Copyright © 2018年 X Young. All rights reserved.
+//
+
+import Foundation
+
+/// 音色枚举
+enum Timbres: Int {
+    case paino = 0, pad, bass, drum
+}
