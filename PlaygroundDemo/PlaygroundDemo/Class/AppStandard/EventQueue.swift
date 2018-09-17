a887494709b81f122282c22eded4ff9b8af905d6
seaniezhao <zwxbest@hotmail.com>
2018-09-17 13:12:41 +0800

    增加que

---
+++ PlaygroundDemo/PlaygroundDemo/Class/AppStandard/EventQueue.swift
@@ -0,0 +1,13 @@
+//
+//  EventQueue.swift
+//  PlaygroundDemo
+//
+//  Created by apple on 2018/9/17.
+//  Copyright © 2018年 X Young. All rights reserved.
+//
+
+import UIKit
+
+class EventQueue: NSObject {
+
+}
