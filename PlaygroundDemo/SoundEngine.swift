6e15343904f800dc6f55dc5e6adcbe95269a2552
seaniezhao <zwxbest@hotmail.com>
2018-09-17 12:35:21 +0800

    增加声音控制引擎

--- PlaygroundDemo/SoundEngine.swift
+++ PlaygroundDemo/SoundEngine.swift
@@ -7,7 +7,38 @@
 //
 
 import UIKit
+import AudioKit
 
 class SoundEngine: NSObject {
-
+    
+    var mixer  = AKMixer()
+    var timbreDict = [Int:AKMIDISampler]()
+    
+    
+    override init(){
+        
+        
+        AudioKit.output = mixer
+        do {
+            try AudioKit.start()
+        } catch {
+            AKLog("AudioKit did not start!")
+        }
+        
+        super.init()
+    }
+    
+    func GetSampler(timbre:Int) -> AKMIDISampler{
+  
+        let returnValue = self.timbreDict[timbre]
+        return returnValue!
+    }
+    
+    func RegistTimbre(timbre:Int,sampler: AKMIDISampler){
+        self.timbreDict.updateValue(sampler, forKey: timbre)
+        mixer.connect(input: sampler)
+    }
+    
+    
+    
 }
---
+++ PlaygroundDemo/TimbreManager.swift
@@ -0,0 +1,13 @@
+//
+//  TimbreManager.swift
+//  PlaygroundDemo
+//
+//  Created by apple on 2018/9/17.
+//  Copyright © 2018年 X Young. All rights reserved.
+//
+
+import UIKit
+
+class TimbreManager: NSObject {
+
+}
